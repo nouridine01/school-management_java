cd87140ac4d367e24eeee7f4591ecbaeb7e81a4e
BAKO ADAMOU <mbariki.bako@2isoft.com>
2020-06-06 19:42:08 +0100

    AJOUT DES SETTERS ET DES GETTERS

--- src/main/java/com/noor/school/entities/Note.java
+++ src/main/java/com/noor/school/entities/Note.java
@@ -30,5 +30,62 @@ public class Note {
 		this.id_module = id_module;
 		this.commentaire = commentaire;
 	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public long getId_etudiant() {
+		return id_etudiant;
+	}
+
+	public void setId_etudiant(long id_etudiant) {
+		this.id_etudiant = id_etudiant;
+	}
+
+	public long getId_enseigant() {
+		return id_enseigant;
+	}
+
+	public void setId_enseigant(long id_enseigant) {
+		this.id_enseigant = id_enseigant;
+	}
+
+	public long getId_module() {
+		return id_module;
+	}
+
+	public void setId_module(long id_module) {
+		this.id_module = id_module;
+	}
+
+	public String getCommentaire() {
+		return commentaire;
+	}
+
+	public void setCommentaire(String commentaire) {
+		this.commentaire = commentaire;
+	}
+
+	public Date getCreated_at() {
+		return created_at;
+	}
+
+	public void setCreated_at(Date created_at) {
+		this.created_at = created_at;
+	}
+
+	public Date getModified_at() {
+		return modified_at;
+	}
+
+	public void setModified_at(Date modified_at) {
+		this.modified_at = modified_at;
+	}
+	
 	
 }
